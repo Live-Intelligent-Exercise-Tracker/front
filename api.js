bcf8f69f87db41911157075caa6d7bdec5315057
jeonha01 <kangjeonha@gmail.com>
2025-02-13 02:05:49 +0900

    2/13 로그인페이지 UI수정

--- api.js
+++ api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import { Platform } from "react-native";
+
+const API_BASE_URL =
+  Platform.OS === "android"
+    ? "http://10.0.2.2:5000/"  // Android 에뮬레이터에서는 이렇게 설정
+    : "http://127.0.0.1:5000/"; // iOS 및 웹에서는 이렇게 설정
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: API_BASE_URL,
 });
 
 api.interceptors.request.use(
--- pages/Login/Login.jsx
+++ pages/Login/Login.jsx
@@ -3,6 +3,8 @@ import {
   StyleSheet, Text, View, TextInput, TouchableOpacity,
   Keyboard, TouchableWithoutFeedback, Image
 } from 'react-native';
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
+import { LinearGradient } from 'expo-linear-gradient'
 import api from '../../api';
 
 //flask run <- 서버 여는 코드
@@ -29,6 +31,7 @@ export default function Login() {
 
       if (!response.data || !user_id || !token) {
         setMessage("로그인에 실패했습니다.");
+        console.log(message)
         return;
       }
 
@@ -64,25 +67,34 @@ export default function Login() {
   };
 
   return (
+
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={styles.container}>
+      <LinearGradient
+        colors={['#000022', '#000000', '#0000AA']} // 검은색 → 약한 남색 → 진한 파란색
+        locations={[0, 0.7, 1]} // 30%까지 검은색, 이후 점점 파란색
+        start={{ x: 0, y: 0 }} // 왼쪽 위에서 시작
+        end={{ x: 1, y: 1 }} // 오른쪽 아래로 진행
+        style={styles.container}
+      >
 
         <View style={styles.textContainer}>
-          <Text style={styles.text}>실시간 지능형{'\n'}운동 추적기</Text>
+          <Text style={styles.text}>데이터로 설계하는{'\n'}실시간 지능형 운동 추적기</Text>
         </View>
-        <Text style={styles.text1}>아이디</Text>
+
+        {/* <Image
+          source={require('../../assets/spectrum.png')}
+          style={styles.spectrum}
+        /> */}
+
         <TextInput
           style={styles.input}
           placeholder="아이디를 입력하세요"
           placeholderTextColor="#D3D3D3"
           value={email}
-          onChangeText={(text) => {
-            const filteredText = text.replace(/[^a-zA-Z0-9@._-]/g, ""); // 영어, 숫자, 일부 특수문자만 허용
-            setEmail(filteredText);
-          }}
+          onChangeText={setEmail}
           keyboardType="email-address"
         />
-        <Text style={styles.text1}>비밀번호</Text>
+
         <TextInput
           style={styles.input}
           placeholder="비밀번호를 입력하세요"
@@ -94,16 +106,16 @@ export default function Login() {
         {/* 아이디 찾기 | 비밀번호 찾기 */}
         <View style={styles.findContainer}>
           <View style={styles.account}>
-            <TouchableOpacity onPress={handleFindId} activeOpacity={1}>
+            <TouchableOpacity onPress={handleFindId}>
               <Text style={styles.findText}>아이디 찾기</Text>
             </TouchableOpacity>
             <Text style={styles.findSeparator}> | </Text>
-            <TouchableOpacity onPress={handleFindPassword} activeOpacity={1}>
+            <TouchableOpacity onPress={handleFindPassword}>
               <Text style={styles.findText}>비밀번호 찾기</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.signup}>
-            <TouchableOpacity onPress={handleSignUp} activeOpacity={1}>
+            <TouchableOpacity onPress={handleSignUp}>
               <Text style={styles.signUpText}>회원가입</Text>
             </TouchableOpacity>
           </View>
@@ -111,30 +123,36 @@ export default function Login() {
         <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.buttonText}>회원 로그인</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, { backgroundColor: '#FDE500', flexDirection: 'row', alignItems: 'center' }]} onPress={handleKakaoLogin}>
+        <TouchableOpacity style={[styles.button, { backgroundColor: '#FDE500', flexDirection: 'row' }]} onPress={handleKakaoLogin}>
           <Image
             source={require('../../assets/kakao_icon.png')} // 이미지 경로 맞춰줘야 함
             style={styles.kakaoIcon}
           />
-          <Text style={[styles.buttonText, { color: '#381F1F', marginLeft: 65 }]}>카카오톡 로그인</Text>
+          <Text style={[styles.buttonText, { color: '#381F1F', left: moderateScale(-10) }]}>카카오톡 로그인</Text>
         </TouchableOpacity>
-      </View>
+      </LinearGradient>
     </TouchableWithoutFeedback>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#161616',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: '40'
+    padding: moderateScale(5)
   },
+  // spectrum: {
+  //   backgroundColor: 'black',
+  //   marginBottom: '50'
+  // },
   kakaoIcon: {
-    width: 24,  // 아이콘 크기 조절
-    height: 24,
+    width: moderateScale(24),  // 아이콘 크기 조절
+    height: moderateScale(24),
     resizeMode: 'contain',  // 비율 유지
+    left: moderateScale(-75),
   },
   textContainer: {
     flexDirection: 'row',
@@ -142,38 +160,30 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   text: {
-    color: '#53E6DE',
-    fontSize: 26,
-    marginBottom: 20,
-    fontWeight: 'bold',
+    color: 'white',
+    fontSize: moderateScale(26),
+    marginBottom: moderateScale(20),
+    fontWeight: 'semibold',
     flexDirection: 'row',
     flex: 1, // 왼쪽 영역 차지
   },
-  text1: {
-    marginTop: 10,
-    marginBottom: 5,
-    color: 'white',
-    fontSize: '16',
-    alignSelf: 'flex-start',  // Text를 왼쪽으로 정렬
-    width: '100%',  // 전체 너비로 맞추기
-  },
   input: {
     width: '100%',
-    padding: 10,
-    marginBottom: 5,
-    backgroundColor: '#161616',
+    padding: moderateScale(10),
+    marginBottom: moderateScale(15),
+    backgroundColor: '#171717',
     color: 'white',
     borderRadius: 3,
-    borderColor: '#929292',
+    borderColor: 'white',
     borderWidth: 1,
-    fontSize: 16,
+    fontSize: moderateScale(16),
+    height: moderateScale(50),
   },
   findContainer: {
     flexDirection: 'row',  // 가로 정렬
     justifyContent: 'space-between', // 양 끝 정렬
     alignItems: 'center',  // 세로 정렬 맞추기
-    marginTop: 10,
-    marginBottom: 10,
+    marginBottom: moderateScale(10),
     width: '100%',
   },
   account: {
@@ -187,28 +197,28 @@ const styles = StyleSheet.create({
   },
   findText: {
     color: '#BFBFBF',
-    fontSize: 15,
+    fontSize: moderateScale(15),
   },
   findSeparator: {
     color: '#BFBFBF',
-    fontSize: 15,
-    marginHorizontal: 5,
+    fontSize: moderateScale(15),
+    marginHorizontal: moderateScale(5),
   },
   button: {
-    marginTop: 20,
+    marginTop: moderateScale(20),
     width: '100%',
-    height: 46,
-    padding: 14,
-    backgroundColor: '#53E6DE',
+    height: moderateScale(50),
+    justifyContent: 'center',
+    backgroundColor: '#2A79FF',
     alignItems: 'center',
     borderRadius: 3,
   },
   buttonText: {
     color: 'white',
-    fontSize: 16,
+    fontSize: moderateScale(16),
   },
   signUpText: {
     color: '#BFBFBF',
-    fontSize: 15,
+    fontSize: moderateScale(15),
   },
 });
