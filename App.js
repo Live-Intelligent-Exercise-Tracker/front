c884b88786cd49531d64dcf3b892cdb0d2c9bd0c
jeonha01 <kangjeonha@gmail.com>
2025-02-21 14:54:33 +0900

    2/21 약관동의 UI 개발완료

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ function MainTabs() {
         component={My}
         options={{
           tabBarIcon: ({ focused }) => (
-            <Ionicons name="person-outline" size={moderateScale(24)} color={focused ? "#FF5733" : "#505050"} />
+            <Ionicons name="person-outline" size={moderateScale(24)} color={focused ? "#507DFA" : "#505050"} />
           ),
         }}
       />
@@ -34,7 +34,7 @@ function MainTabs() {
         component={Main}
         options={{
           tabBarIcon: ({ focused }) => (
-            <Ionicons name="home-outline" size={moderateScale(24)} color={focused ? "#FF5733" : "#505050"} />
+            <Ionicons name="home-outline" size={moderateScale(24)} color={focused ? "#507DFA" : "#505050"} />
           ),
         }}
       />
@@ -43,7 +43,7 @@ function MainTabs() {
         component={Group}
         options={{
           tabBarIcon: ({ focused }) => (
-            <Ionicons name="people-outline" size={moderateScale(24)} color={focused ? "#FF5733" : "#505050"} />
+            <Ionicons name="people-outline" size={moderateScale(24)} color={focused ? "#507DFA" : "#505050"} />
           ),
         }}
       />
@@ -56,7 +56,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="Agree" component={Agree} options={{ title: "약관 동의" }} />
+        <Stack.Screen name="Agree" component={Agree} options={{
+          title: "약관 동의",
+          headerStyle: { backgroundColor: '#161616', shadowOpacity: 0, elevation: 0, borderBottomWidth: 0, },
+          headerTintColor: "#FFFFFF",
+          headerBackTitle: ''
+        }} />
         <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
--- pages/SignUp/Agree.jsx
+++ pages/SignUp/Agree.jsx
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { moderateScale } from 'react-native-size-matters';
+import Checkbox from "expo-checkbox";
 
 export default function Agree({ navigation }) {
+    const [isChecked, setIsChecked] = useState(false);
+
     return (
         <View style={styles.container}>
-            <Text>약관동의</Text>
-            <StatusBar style="auto" />
+            <Text style={[styles.text, { marginBottom: moderateScale(20) }]}>약관동의</Text>
+            <View style={styles.clauses}>
+                <Checkbox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+                <Text style={[styles.text, { fontSize: moderateScale(14) }]}>전체 동의</Text>
+            </View>
+            <View style={{ height: moderateScale(0.5), backgroundColor: "#B3B3B3", marginBottom: moderateScale(20) }} />
+            <View style={styles.clauses}>
+                <Checkbox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+                <Text style={[styles.text, { fontSize: moderateScale(14), fontWeight: 'normal' }]}>만 14세 이상입니다.</Text>
+                <Text style={styles.essential}>(필수)</Text>
+            </View>
+            <View style={styles.clauses}>
+                <Checkbox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+                <Text style={[styles.text, { fontSize: moderateScale(14), fontWeight: 'normal' }]}>이용약관 동의</Text>
+                <Text style={styles.essential}>(필수)</Text>
+                <View style={{ justifyContent: 'flex-end', flex: 1, flexDirection: 'row' }}>
+                    <Text style={styles.termLook}>약관보기</Text>
+                </View>
+            </View>
+            <View style={styles.clauses}>
+                <Checkbox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+                <Text style={[styles.text, { fontSize: moderateScale(14), fontWeight: 'normal' }]}>개인정보 수집 및 이용 동의</Text>
+                <Text style={styles.essential}>(필수)</Text>
+                <View style={{ justifyContent: 'flex-end', flex: 1, flexDirection: 'row' }}>
+                    <Text style={styles.termLook}>약관보기</Text>
+                </View>
+            </View>
+            <View style={styles.clauses}>
+                <Checkbox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+                <Text style={[styles.text, { fontSize: moderateScale(14), fontWeight: 'normal' }]}>개인정보 마케팅 활용 동의</Text>
+                <Text style={[styles.essential, { color: '#B3B3B3' }]}>(선택)</Text>
+            </View>
+            <View style={styles.clauses}>
+                <Checkbox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+                <Text style={[styles.text, { fontSize: moderateScale(14), fontWeight: 'normal' }]}>이벤트 알림 메일 및 SMS 등 수신</Text>
+                <Text style={[styles.essential, { color: '#B3B3B3' }]}>(선택)</Text>
+            </View>
+            <View style={{ justifyContent: 'flex-end', flex: 1, }}>
+                <TouchableOpacity style={{ borderRadius: 10, backgroundColor: '#B3B3B3', height: moderateScale(49.74), justifyContent: 'center', alignItems: 'center' }}>
+                    <Text style={{ fontSize: moderateScale(16), color: '#FFFFFF', fontWeight: 'bold' }}>다음</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
-
+// 트러블 슈팅 체크박스?
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#161616',
+        padding: moderateScale(20)
     },
+    text: {
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        fontSize: moderateScale(16),
+    },
+    checkbox: {
+        width: moderateScale(24),
+        height: moderateScale(24),
+        borderColor: '#FFFFFF',
+        marginRight: moderateScale(10)
+    },
+    clauses: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: moderateScale(20)
+    },
+    essential: {
+        color: '#0085FF',
+        fontSize: moderateScale(8),
+        marginLeft: moderateScale(10)
+    },
+    termLook: {
+        fontWeight: 'bold',
+        color: '#B3B3B3',
+        fontSize: moderateScale(8),
+    }
 });
